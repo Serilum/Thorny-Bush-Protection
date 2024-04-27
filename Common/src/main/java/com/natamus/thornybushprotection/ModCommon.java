1e8e8e6fb05392815047af555e1c74e86d73c161
Rick South <rick@natamus.com>
2024-04-28 01:22:32 +0200

    Thorny Bush Protection source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/thornybushprotection/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.thornybushprotection;
+
+import com.natamus.thornybushprotection.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
