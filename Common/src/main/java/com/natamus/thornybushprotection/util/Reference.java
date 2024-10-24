dc1ffd223499fa2f2d1f1f9a9d311db5b777e8f1
Rick South <rick@natamus.com>
2024-10-24 17:58:01 +0200

    Thorny Bush Protection source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/thornybushprotection/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.thornybushprotection.util;
+
+public class Reference {
+	public static final String MOD_ID = "thornybushprotection";
+	public static final String NAME = "Thorny Bush Protection";
+	public static final String VERSION = "1.4";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
