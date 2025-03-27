dc740c24b4d8c321b8f97d51792a3fe387b413fe
Rick South <rick@natamus.com>
2025-03-28 00:27:16 +0100

    (v1.5) Fixed incorrect mixin reference.

--- Common/src/main/java/com/natamus/thornybushprotection/mixin/SweetBerryBushMixin.java
+++ Common/src/main/java/com/natamus/thornybushprotection/mixin/SweetBerryBushMixin.java
@@ -3,6 +3,7 @@ package com.natamus.thornybushprotection.mixin;
 import com.natamus.thornybushprotection.util.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = SweetBerryBushBlock.class, priority = 1001)
 public class SweetBerryBushMixin {
-	@Inject(method = "entityInside(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"), cancellable = true)
-	public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity, CallbackInfo ci) {
+	@Inject(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurtServer(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)Z"), cancellable = true)
+	public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity, InsideBlockEffectApplier insideBlockEffectApplier, CallbackInfo ci) {
 		if (Util.shouldCancelBushDamage(entity)) {
 			ci.cancel();
 		}
