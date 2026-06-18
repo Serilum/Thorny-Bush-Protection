03626310fd9f680e8152e2f2e5bd405d0664a28f
Rick South <rick@natamus.com>
2026-06-18 21:16:30 +0200

    Updated Thorny Bush Protection to Minecraft version 26.2.0.

--- Forge/src/main/java/com/natamus/thornybushprotection/ModForge.java
+++ Forge/src/main/java/com/natamus/thornybushprotection/ModForge.java
@@ -5,7 +5,6 @@ import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.thornybushprotection.forge.config.IntegrateForgeConfig;
 import com.natamus.thornybushprotection.util.Reference;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
