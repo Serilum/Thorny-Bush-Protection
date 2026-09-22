cfabc4ac673abe12ae39054bddd6854d67e0a21d
Rick South <rick@natamus.com>
2026-09-22 15:59:45 +0200

    Updated Thorny Bush Protection to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/thornybushprotection/util/Reference.java
+++ Common/src/main/java/com/natamus/thornybushprotection/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "thornybushprotection";
 	public static final String NAME = "Thorny Bush Protection";
 	public static final String VERSION = "1.6";
-	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
+	public static final String ACCEPTED_VERSIONS = "[26.3.0]";
 }
